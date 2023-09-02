f36ff77bfa2724791c8fdc6018714f8c700d24e9
Tom Scerri <hello@deeplift.dev>
2023-09-02 21:40:36 +1000

    Fix for vercel.

--- src/app/(components)/animals-menu.tsx
+++ src/app/(components)/animals-menu.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { cookies } from "next/headers";
 import AnimalsSelector from "./animals-selector";
-import AddAnimalDialog from "./dialogs/add-animal-dialog";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export default async function AnimalsMenu() {
-  const supabase = createServerComponentClient<Database>({ cookies });
+  const cookieStore = cookies()
+  const supabase = createServerComponentClient<Database>({
+    cookies: () => cookieStore
+  })
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
   const { data: animals } = await supabase
     .from("animals")
     .select("*, profiles(*)");
