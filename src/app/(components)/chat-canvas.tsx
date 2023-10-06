b7c617bfccd1625abe88ee1d306adde3731ec7ea
Tom Scerri <hello@deeplift.dev>
2023-10-06 17:32:02 +1100

    Style fix for mobile

--- src/app/(components)/chat-canvas.tsx
+++ src/app/(components)/chat-canvas.tsx
@@ -56,7 +56,7 @@ export default function Chat() {
     <div className="flex h-full">
       <div className="flex overflow-hidden w-full">
         <div className="flex flex-col h-screen overflow-hidden w-full">
-          <ScrollToBottom className="flex flex-col text-sm dark:bg-gray-800 overflow-scroll h-full no-scrollbar py-24">
+          <ScrollToBottom className="flex flex-col text-sm dark:bg-gray-800 overflow-scroll h-full no-scrollbar mt-40">
             {messages.map((message) => (
               <ConversationMessage
                 key={message.id}
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -24,7 +24,7 @@ export default async function DashboardLayout({
   return (
     <main>
       <div>
-        <GradientBackground />
+        {/* <GradientBackground /> */}
         <MainNav />
         <div>{children}</div>
       </div>
